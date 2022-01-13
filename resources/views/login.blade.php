80b069ce38c3cf8579fa5820642ca914fef32584
Truong Duong Khang <truongduongkhang277@gmail.com>
2022-01-13 14:11:42 +0700

    commit

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,11 +10,11 @@
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="/adminlte/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css')}}">
     <!-- icheck bootstrap -->
-    <link rel="stylesheet" href="adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="adminlte/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css')}}">
 </head>
 
 <body class="hold-transition login-page">
@@ -71,11 +71,11 @@
     <!-- /.login-box -->
 
     <!-- jQuery -->
-    <script src="/adminlte/plugins/jquery/jquery.min.js"></script>
+    <script src="{{ asset('adminlte/plugins/jquery/jquery.min.js')}}"></script>
     <!-- Bootstrap 4 -->
-    <script src="/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
     <!-- AdminLTE App -->
-    <script src="/adminlte/dist/js/adminlte.min.js"></script>
+    <script src="{{ asset('adminlte/dist/js/adminlte.min.js')}}"></script>
 
 </body>
 
